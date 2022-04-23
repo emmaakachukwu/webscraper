89ae4813247d535b37c022f7e34d9ef86fec4e47
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2022-04-23 15:26:18 +0100

    cleanup.. scraper working

--- index.js
+++ index.js
@@ -10,23 +10,17 @@ const __dirname = dirname(__filename)
 const { url, dir } = minimist(process.argv)
 
 scrape({
-    // Provide the URL(s) of the website(s) that you want to clone
-    // In this example, you can clone the Our Code World website
     urls: [url],
-    // Specify the path where the content should be saved
-    // In this case, in the current directory inside the ourcodeworld dir
     directory: path.resolve(__dirname, dir),
-    // Load the Puppeteer plugin
     plugins: [ 
         new PuppeteerPlugin({
             launchOptions: { 
-                // If you set  this to true, the headless browser will show up on screen
                 headless: true
-            }, /* optional */
+            },
             scrollToBottom: {
                 timeout: 10000, 
                 viewportN: 10 
-            } /* optional */
+            }
         })
     ]
 })
