02784d8c4e2c5cf3b006733eea8518fcde7ad1c1
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2022-04-23 08:40:37 +0100

    setup

---
+++ index.js
@@ -0,0 +1,9 @@
+import scrape from "website-scraper"
+import PuppeteerPlugin from "website-scraper-puppeteer"
+import path from "path"
+import {fileURLToPath} from "url"
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
